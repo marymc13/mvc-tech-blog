a55c3afa049aaed1e114e71ac63bb76c70014669
Mary Paul <marymc135@hotmail.com>
2022-10-12 12:33:38 -0400

    add comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,8 +29,11 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
       // pass a single post object into the homepage template
-      res.render('homepage', { posts });
+      res.render('homepage',  {
+       posts, 
+       loggedIn:req.session.loggedIn
     })
+  })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
